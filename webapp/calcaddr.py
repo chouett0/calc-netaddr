132b8b1130bfdd2b14bab692094467432a1ff2c5
Ubuntu <chouett0.rev@gmail.com>
2023-07-05 12:18:42 +0000

    Changed to original implementation method without ChatGPT

--- webapp/calcaddr.py
+++ webapp/calcaddr.py
@@ -9,6 +9,7 @@ Blueprint, flash, g, redirect, render_template, request, session, url_for
 
 bp = Blueprint('calcaddr', __name__, url_prefix='/calcaddr')
 
+"""
 class ChatGPT:
 	def __init__(self):
 		# ChatGPT API KEY
@@ -46,10 +47,100 @@ class ChatGPT:
 
 		return res
 
+"""
+
+class CalcIPAddress:
+	def __init__(self):
+		self.subnet_bin	= ''
+		self.netaddr_bin	= ''
+		self.bcaddr_bin	= ''
+
+		self.subnet		= ''
+		self.netaddr	= ''
+		self.bcaddr		= ''
+
+		self.ip_bin		= ''
+		self.prefix 	= ''
+		self.can_use_ip = 0
+
+	## サブネット
+	def calcSubnet(self):
+		self.subnet		= ''
+
+		self.subnet_bin = '1'*int(self.prefix) + '0'*(32 - self.prefix)
+		for i in range(0, 32, 8):
+			if ( i == 24 ):
+				self.subnet += str(int('0b' + self.subnet_bin[i:i+8], 0))
+			else:
+				self.subnet += str(int('0b' + self.subnet_bin[i:i+8], 0)) + '.'
+
+
+
+	## ネットワークアドレス
+	def calcNetworkAddress(self):
+		self.netaddr	= ''
+
+		self.netaddr_bin = bin(int('0b' + self.subnet_bin, 0) & int('0b' + self.ip_bin, 0))[2:].zfill(32)
+		for i in range(0, 32, 8):
+			if ( i == 24 ):
+				self.netaddr += str( int('0b' + self.netaddr_bin[i:i+8], 0) )
+			else:
+				self.netaddr += str( int('0b' + self.netaddr_bin[i:i+8], 0) ) + '.'
+
+
+	## ブロードキャストアドレス
+	def calcBroadcastAddress(self):
+		self.bcaddr		= ''
+
+		self.can_use_ip = int('0b' + str(('1'*(32 - self.prefix)).zfill(32)), 0) - 1
+		self.bcaddr_bin = bin(int('0b' + self.netaddr_bin, 0) | int('0b' + str(('1'*(32 - self.prefix)).zfill(32)), 0))[2:].zfill(32)
+		for i in range(0, 32, 8):
+			if ( i == 24 ):
+				self.bcaddr += str( int('0b' + self.bcaddr_bin[i:i+8], 0) )
+			else:
+				self.bcaddr += str( int('0b' + self.bcaddr_bin[i:i+8], 0) ) + '.'
+
+
+	def calc(self, addr_list):
+		result = "## Result\n"
+
+		for addr in addr_list:
+			self.ip_bin = ''
+			self.can_use_ip = 0
+
+			prefix = re.findall('\d+\.\d+\.\d+\.\d+\/(\d+)', addr)
+			if ( len(prefix) <= 0 ):
+				result += "[!] Invalid IP Address Prefix: {}\n\n".format(addr)
+				continue
+
+			else:
+				self.prefix = int(prefix[0])
+
+			ip_addr = [x for x in addr.split('/')[0].split('.')]
+			if ( len(ip_addr) < 4 ):
+				result += "[!] Invalid IP Address: {}\n\n".format(addr)
+				continue
+
+			for ip in ip_addr:
+				self.ip_bin += bin(int(ip))[2:].zfill(8)
+
+			print('[*] address list: {}'.format(addr_list))
+			print('[*] ip address(bin): {}'.format(self.ip_bin))
+			print('[*] Prefix: {}'.format(self.prefix))
+
+			self.calcSubnet()
+			self.calcNetworkAddress()
+			self.calcBroadcastAddress()
+
+			result += 'IP Address: {}\nSubnet Mask: {}\nNetwork Address: {}\nBroadcast Address: {}\nCan Use IP Address: {}\n\n'.format(addr, self.subnet, self.netaddr, self.bcaddr, self.can_use_ip)
+
+		return result
+
 
 @bp.route('/calc', methods=('GET', 'POST'))
 def calc():
-	gpt = ChatGPT()
+#	gpt = ChatGPT()
+	calc_ip = CalcIPAddress()
 	error = ""
 
 	if ( request.method == 'POST' ):
@@ -64,7 +155,8 @@ def calc():
 				error = "IP Address is Not Found."
 
 			else:
-				res = gpt.calcNetworkAddr(addr_list)
+#				res = gpt.calcNetworkAddr(addr_list)
+				res = calc_ip.calc(addr_list)
 				return render_template('calcaddr.html', answer=res)
 
 		else:
